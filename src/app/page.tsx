ab00dd6c26aa3a3ad79215b138f37a6a3965bfe9
professorabhay <abhayetwup123@gmail.com>
2024-03-06 15:05:55 +0530

    NextJs-Chai_Aur_Code

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MovingCards from "@/components/MovingCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MovingCards />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
+    </main>
+  );
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,92 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-black text-gray-400 py-12'>
+        <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8">
+            {/* 1st */}
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">About Us</h2>
+          <p className="mb-4">
+            Music School is a premier institution dedicated to teaching the art
+            and science of music. We nurture talent from the ground up,
+            fostering a vibrant community of musicians.
+          </p>
+        </div>
+        {/* 2nd */}
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Quick Links</h2>
+          <ul>
+            <li>
+              <a
+                href="#"
+                className="hover:text-white transition-colors duration-300"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="hover:text-white transition-colors duration-300"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="hover:text-white transition-colors duration-300"
+              >
+                Courses
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="hover:text-white transition-colors duration-300"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        {/* 3rd */}
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Follow Us</h2>
+          <div className="flex space-x-4">
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Facebook
+            </a>
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Twitter
+            </a>
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Instagram
+            </a>
+          </div>
+        </div>
+        {/* 4th */}
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Contact Us</h2>
+          <p>Jhansi, India</p>
+          <p>Jhansi 284128</p>
+          <p>Email: ayush87269@gmail.com</p>
+          <p>Phone: (123) 456-7890</p>
+        </div>
+        </div>
+        <p className="text-center text-xs pt-8">© 2024 Music Courses. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer;
